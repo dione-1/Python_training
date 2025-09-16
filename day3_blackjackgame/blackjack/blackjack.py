dce2ddfedd1abeb83e7bb2b3afa0f0911400cd4f
dione <dione-1@ya.ru>
2025-09-16 18:49:55 +0200

    Refactor blackjack script: add `Hand`, `Card`, and `Deck` classes to encapsulate logic, streamline card dealing, and improve code readability.

--- day3_blackjackgame/blackjack/blackjack.py
+++ day3_blackjackgame/blackjack/blackjack.py
@@ -1,36 +1,85 @@
 import random
 
+class Hand:
+    def __init__(self, dealer=False):
+        self.cards = []
+        self.value = 0
 
-cards = []
-suits = ["spades", "hearts", "clubs", "diamonds"]
-ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
+        self.dealer = dealer
+    def add_card(self, card):
+        self.cards.extend(card)
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            card.value = int(card.rank["value"])
+            self.value += card.value
+            if card.rank["rank"] == "A":
+                has_ace = True
+        if has_ace and self.value > 21:
+            self.value -= 10
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+    def is_blackjack(self):
+        return self.value == 21
+    def display(self):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand''')
+        for card in self.cards:
+            print(card)
 
-for suit in suits:
-    for rank in ranks:
-        cards.append([suit, rank])
-
-def shuffle():
-    random.shuffle(cards)
-
-def deal(number):
-    cards_dealt = []
-    for x in range(number):
-        card = cards.pop()
-        cards_dealt.append(card)
-    return cards_dealt
-
-shuffle()
-cards_dealt = deal(2)
-card = cards_dealt[0]
-rank = card[1]
-if rank == "A":
-    value = 11
-elif rank == "J" or rank == "Q" or rank == "K":
-    value = 10
-else:
-    value = rank
-
-rank_dict = {"rank" : rank, "value" : value}
+        if not self.dealer:
+            print("Value:", self.get_value())
+        print()
 
 
-print(rank_dict["value"], rank_dict["rank"])
+class Card:
+    def __init__(self, suit, rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
+
+class Deck:
+    def __init__(self):
+        self.cards = []
+        suits = ["spades", "hearts", "clubs", "diamonds"]
+        ranks = [{"rank": "A", "value": 11},
+             {"rank": "K", "value": 10},
+             {"rank": "Q", "value": 10},
+             {"rank": "J", "value": 10},
+             {"rank": "10", "value": 10},
+             {"rank": "9", "value": 9},
+             {"rank": "8", "value": 8},
+             {"rank": "7", "value": 7},
+             {"rank": "6", "value": 6},
+             {"rank": "5", "value": 5},
+             {"rank": "4", "value": 4},
+             {"rank": "3", "value": 3},
+             {"rank": "2", "value": 2}
+             ]
+        for suit in suits:
+            for rank in ranks:
+                self.cards.append(Card(suit, rank))
+    def shuffle(self):
+        if len(self.cards) > 1 :
+            random.shuffle(self.cards)
+    def deal(self, number):
+        cards_dealt = []
+        for x in range(number):
+            if len(self.cards) > 0:
+                card = self.cards.pop()
+                cards_dealt.append(card)
+        return cards_dealt
+
+
+
+
+
+deck = Deck()
+deck.shuffle()
+
+hand = Hand()
+hand.add_card(deck.deal(2))
+
+print(hand.display())
